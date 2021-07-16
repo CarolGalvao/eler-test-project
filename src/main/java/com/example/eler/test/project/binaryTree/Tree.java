bcc610be4e6076336f7665bafbf4f12efa1d9ed5
CarolGalvao <galvao.carolina@hotmail.com>
2021-07-15 21:40:37 -0300

    Sequência de restrições

--- src/main/java/com/example/eler/test/project/binaryTree/Tree.java
+++ src/main/java/com/example/eler/test/project/binaryTree/Tree.java
@@ -3,9 +3,14 @@ package com.example.eler.test.project.binaryTree;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Component
 @Data
 public class Tree {
+
+    ArrayList<String[]> paths = new ArrayList<>();
 
     public void setTree(String javaFile){
         String[] arrays = javaFile.split("\n");
@@ -21,16 +26,21 @@ public class Tree {
                 }
             }
         }
+
+        System.out.println(" ");
         System.out.println("Grafo: ");
         printTree(root);
-        String path[] = new String[1000];
+        String path[] = new String[15];
+        System.out.println(" ");
         System.out.println("Identificar caminhos: ");
-        printPathsRecur(root, path, 0);
+        printPaths(root, path, 0);
+        System.out.println(" ");
         System.out.println("Sequência de restrições: ");
+        printRestriction();
     }
 
     public String cleanLine(String line) {
-       if(line.contains("package") || line.contains("class") || line.isEmpty() || line.contains("}")) {
+       if(line.contains("package") || line.contains("class") || line.isEmpty() || line.contains("}") || line.contains("//")) {
            return null;
        }
        if(line.contains("public")) {
@@ -50,7 +60,7 @@ public class Tree {
         newNode.setValue(value);
         newNode.setLeaf(newNode.getValue().contains("return"));
         if (previous != null) {
-            if( !previous.isLeaf() || previous.getPrior() == null){
+            if( !previous.isLeaf() || previous.getPrior() == null) {
                 previous.setLeft(newNode);
                 newNode.setPrior(previous);
             }else {
@@ -62,8 +72,8 @@ public class Tree {
     }
 
     //Mostra a arvore em ordem simétrica
-    public void printTree (Node node){
-        if( node != null ){
+    public void printTree (Node node) {
+        if( node != null ) {
             System.out.println(node.getValue());
             printTree(node.getLeft());
             printTree(node.getRight());
@@ -71,29 +81,50 @@ public class Tree {
     }
 
     //Mostrar todos os caminhos da arvore
-    void printPathsRecur(Node node, String path[], int pathLen)
-    {
+    void printPaths(Node node, String path[], int pathLen) {
         if (node == null) {
             return;
         }
-        path[pathLen] = node.getValue();
+        String pathClone[] = path.clone();
+        pathClone[pathLen] = node.getValue();
         pathLen++;
 
-        if (node.getLeft() == null && node.getRight() == null){
-            printArray(path, pathLen);
+        if (node.isLeaf()) {
+            printArray(pathClone, pathLen, node);
         } else {
-            printPathsRecur(node.getLeft(), path, pathLen);
-            printPathsRecur(node.getRight(), path, pathLen);
+            printPaths(node.getLeft(), pathClone, pathLen);
+            printPaths(node.getRight(), pathClone, pathLen);
         }
     }
 
-    void printArray(String ints[], int len)
-    {
-        int i;
-        for (i = 0; i < len; i++) {
-            System.out.print(ints[i] + " ");
+    void printArray(String ints[], int len, Node node ) {
+        for (int i = 0; i < len; i++) {
+            if(i != len - 2 || node.getPrior().getRight().getValue().equals(ints[i + 1]) ){
+                ints[i] = ints[i].replace(">", "<");
+                ints[i] = ints[i].replace("==", "!=");
+                ints[i] = ints[i].replace("(primeiraCompra)", "(!primeiraCompra)");
+            }
+            System.out.print(ints[i]);
         }
+        paths.add(ints);
         System.out.println("");
     }
 
+    //Mostra sequencias de restrições
+    public void printRestriction () {
+        paths.stream().forEach(path ->{
+            Arrays.stream(path).sequential().forEach(
+                    param -> {
+                        if(param == null) return;
+                        if(param.contains("if")){
+                            param = param.replace("if (", "");
+                            param = param.replace(")", "");
+                            System.out.print(param);
+
+                        }
+                    }
+            );
+            System.out.println();
+        });
+    }
 }
--- src/main/resources/Pedido.java
+++ src/main/resources/Pedido.java
@@ -1,13 +1,17 @@
 package codigos;
 
+//bronze = 1
+//prate = 2
+//ouro = 3
+
 public class Pedido {
 
-    public float calculaTaxaDesconto (boolean primeiraCompra, String tipoCliente, float valorCompra) {
+    public float calculaTaxaDesconto (boolean primeiraCompra, int tipoCliente, float valorCompra) {
         if (valorCompra > 500)
             return 15;
-        if (tipoCliente.equals("ouro"))
+        if (tipoCliente == 3)
             return 15;
-        if (tipoCliente.equals("prata"))
+        if (tipoCliente == 2)
             return  10;
         if (primeiraCompra)
             return 10;
@@ -15,7 +19,7 @@ public class Pedido {
             return 10;
         if (valorCompra > 200)
             return 5;
-        if (tipoCliente.equals("bronze"))
+        if (tipoCliente == 1)
             return 5;
 
         return 0;
